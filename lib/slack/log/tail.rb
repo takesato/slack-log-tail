45e6b45e54b4fda78267a65514241ff2933ab96e
takehito sato <takehitosato@gmail.com>
2015-10-13 19:42:29 +0900

    bundle gem slack-log-tail

---
+++ lib/slack/log/tail.rb
@@ -0,0 +1,9 @@
+require "slack/log/tail/version"
+
+module Slack
+  module Log
+    module Tail
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/slack/log/tail/version.rb
@@ -0,0 +1,7 @@
+module Slack
+  module Log
+    module Tail
+      VERSION = "0.1.0"
+    end
+  end
+end
